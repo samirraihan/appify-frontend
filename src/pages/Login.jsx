c0d23b05db9415bc5db59533deaeea9a08602e66
samirraihan <samirraihan93@gmail.com>
2026-04-05 18:04:22 +0600

    feat(auth): enhance registration and login forms with improved UI and validation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -22,42 +22,85 @@ export default function Login() {
 
   return (
     <section className="_social_login_wrapper _layout_main_wrapper">
-      <div className="_social_login_wrap container">
-        <div className="row align-items-center">
-          <div className="col-xl-8 col-lg-8 d-none d-lg-block">
-            <div className="_social_login_left_image">
-              <img src="/assets/images/login.png" alt="Image" className="_left_img" />
-            </div>
-          </div>
-          <div className="col-xl-4 col-lg-4 col-md-12">
-            <div className="_social_login_content">
-              <div className="_social_login_left_logo _mar_b28">
-                <img src="/assets/images/logo.svg" alt="Image" className="_left_logo" />
+      <div className="_shape_one">
+        <img src="/assets/images/shape1.svg" alt="" className="_shape_img" />
+        <img src="/assets/images/dark_shape.svg" alt="" className="_dark_shape" />
+      </div>
+      <div className="_shape_two">
+        <img src="/assets/images/shape2.svg" alt="" className="_shape_img" />
+        <img src="/assets/images/dark_shape1.svg" alt="" className="_dark_shape _dark_shape_opacity" />
+      </div>
+      <div className="_shape_three">
+        <img src="/assets/images/shape3.svg" alt="" className="_shape_img" />
+        <img src="/assets/images/dark_shape2.svg" alt="" className="_dark_shape _dark_shape_opacity" />
+      </div>
+      <div className="_social_login_wrap">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-xl-8 col-lg-8 col-md-12 col-sm-12">
+              <div className="_social_login_left">
+                <div className="_social_login_left_image">
+                  <img src="/assets/images/login.png" alt="Image" className="_left_img" />
+                </div>
               </div>
-              <p className="_social_login_content_para _mar_b8">Welcome back</p>
-              <h4 className="_social_login_content_title _titl4 _mar_b50">Login to your account</h4>
-              <form className="_social_login_form" onSubmit={handleSubmit}>
+            </div>
+            <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12">
+              <div className="_social_login_content">
+                <div className="_social_login_left_logo _mar_b28">
+                  <img src="/assets/images/logo.svg" alt="Image" className="_left_logo" />
+                </div>
+                <p className="_social_login_content_para _mar_b8">Welcome back</p>
+                <h4 className="_social_login_content_title _titl4 _mar_b50">Login to your account</h4>
+                <button type="button" className="_social_login_content_btn _mar_b40">
+                  <img src="/assets/images/google.svg" alt="Image" className="_google_img" /> <span>Or sign-in with google</span>
+                </button>
+                <div className="_social_login_content_bottom_txt _mar_b40"> <span>Or</span>
+                </div>
+                <form className="_social_login_form" onSubmit={handleSubmit}>
+                  <div className="row">
+                    <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                      <div className="_social_login_form_input _mar_b14">
+                        <label className="_social_login_label _mar_b8">Email</label>
+                        <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" className="form-control _social_login_input" required />
+                      </div>
+                    </div>
+                    <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                      <div className="_social_login_form_input _mar_b14">
+                        <label className="_social_login_label _mar_b8">Password</label>
+                        <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" className="form-control _social_login_input" required />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-lg-6 col-xl-6 col-md-6 col-sm-12">
+                      <div className="form-check _social_login_form_check">
+                        <input className="form-check-input _social_login_form_check_input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" defaultChecked />
+                        <label className="form-check-label _social_login_form_check_label" htmlFor="flexRadioDefault2">Remember me</label>
+                      </div>
+                    </div>
+                    <div className="col-lg-6 col-xl-6 col-md-6 col-sm-12">
+                      <div className="_social_login_form_left">
+                        <p className="_social_login_form_left_para">Forgot password?</p>
+                      </div>
+                    </div>
+                  </div>
+                  {error && <div className="text-danger">{error}</div>}
+                  <div className="row">
+                    <div className="col-lg-12 col-md-12 col-xl-12 col-sm-12">
+                      <div className="_social_login_form_btn _mar_t40 _mar_b60">
+                        <button type="submit" className="_social_login_form_btn_link _btn1">Login now</button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
                 <div className="row">
-                  <div className="col-12">
-                    <div className="_social_login_form_input _mar_b14">
-                      <label className="_social_login_label _mar_b8">Email</label>
-                      <input value={email} onChange={(e)=>setEmail(e.target.value)} type="email" className="form-control _social_login_input" required />
-                    </div>
-                  </div>
-                  <div className="col-12">
-                    <div className="_social_login_form_input _mar_b14">
-                      <label className="_social_login_label _mar_b8">Password</label>
-                      <input value={password} onChange={(e)=>setPassword(e.target.value)} type="password" className="form-control _social_login_input" required />
+                  <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                    <div className="_social_login_bottom_txt">
+                      <p className="_social_login_bottom_txt_para">Dont have an account? <Link to="/register">Create New Account</Link>
+                      </p>
                     </div>
                   </div>
                 </div>
-                {error && <div className="text-danger">{error}</div>}
-                <div className="_social_login_form_btn _mar_t40 _mar_b60">
-                  <button type="submit" className="_social_login_form_btn_link _btn1">Login now</button>
-                </div>
-              </form>
-              <div className="_social_login_bottom_txt">
-                <p className="_social_login_bottom_txt_para">Dont have an account? <Link to="/register">Create New Account</Link></p>
               </div>
             </div>
           </div>
